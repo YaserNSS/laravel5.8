c9d467ef212831b91aa1db9f76fc39f89014c3d3
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-02-14 09:21:11 +0300

    Le10: Adding Email For Customers

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -3,19 +3,28 @@
 @section('content')
     <h1>Customers</h1>
     <form action="customers" method="POST" class="pb-5">
-        <div class="input-group">
-            <input type="text" name="name">
+
+        <p>Name :</p>
+        <div class="input-group ob-2">
+            <input type="text" name="name" value="{{old('name')}}">
         </div>
-        <div>    {{$errors->first('name')}}</div>
+        <div>{{$errors->first('name')}}</div>
+
+        <p>Email :</p>
+        <div class="input-group">
+                <input type="email" name="email" value="{{old('email')}}">
+        </div>
+        <div>{{$errors->first('email')}}</div>
+
         <button type="submit" class="btn btn-primary">Add Customer</button>
-                    @csrf
+        @csrf
     </form>
 
 
 
     <ul>
         @foreach ($customers as $customer)
-        <li>{{$customer->name}}</li>
+        <li>{{$customer->name}} (<span class="text-muted">{{$customer->email}}</span>)</li>
         @endforeach
     </ul>
 @endsection
