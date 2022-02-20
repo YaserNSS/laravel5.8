ef898ce07c518d016c4ae185b2640351f7677369
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-02-20 14:36:58 +0300

    Le17: Eloquent Route Model Binding & RESTful Controller - Part 3

---
+++ resources/views/customers/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+
+@section('title','Edit Detials for'. $customer->name)
+
+@section('content')
+
+    <div class="row">
+        <div class="col-12">
+                <h1>Edit Detials for {{$customer->name}}</h1>
+            </div>
+        </div>
+
+    <div class="row">
+        <div class="col-12">
+                <form action="/customers/ {{$customer->id}}" method="POST" class="pb-5">
+                    @method('PATCH')
+                    @include('customers.form')
+                    <button type="submit" class="btn btn-primary">Save Customer</button>
+                </form>
+            </div>
+        </div>
+
+
+
+
+@endsection
+
--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -7,6 +7,7 @@
     <div class="row">
         <div class="col-12">
                 <h1>Detials For {{$customer->name}}</h1>
+                <p><a href="/customers/{{$customer->id}}/edit">Edit</a></p>
             </div>
         </div>
 
