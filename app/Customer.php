bba722405a2e144b106caf8fa0d1cd699286504f
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-02-21 00:43:30 +0300

    Le18: Eloquent Route Model Binding & RESTful Controller - Part 4

--- app/Customer.php
+++ app/Customer.php
@@ -10,12 +10,16 @@ class Customer extends Model
 
     public function getActiveAttribute($attribute)
     {
-        return [
-            0 => 'Inactive',
-            1 => 'Active'
-        ][$attribute];
+        return $this->activeOptions()[$attribute];
 
     }
+
+
+    protected $attributes = [
+        'active' =>1,
+    ]; //default active
+
+
 
     public function scopeActive($query)
     {
@@ -30,5 +34,15 @@ class Customer extends Model
     public function company()
     {
         return $this->belongsTo('App\Company');
+    }
+
+    public function activeOptions()
+    {
+
+        return [
+            1 => 'Active',
+            0 => 'Inactive'
+
+        ];
     }
 }
