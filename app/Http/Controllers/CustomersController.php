fc20a4a733cce38a9cc68b1df74dfa47deec81f1
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-03-29 00:04:26 +0300

    Le 43:Pagination

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -18,7 +18,8 @@ class CustomersController extends Controller
 
     public function index()
     {
-        $customers=Customer::all();
+        // $customers=Customer::all();
+        $customers = Customer::with('company')->paginate(15);
         return view('customers.index',compact('customers'));
 
         // $ActiveCustomers = Customer::active()->get();
